f2c15f192baf01697d0a83a134e48db51cfacbe9
tuyc17 <t2017013579@163.com>
2020-06-19 15:21:04 +0800

    two setting activities finished

--- Project/app/src/main/java/com/example/project/activity/ChangeNicknameActivity.java
+++ Project/app/src/main/java/com/example/project/activity/ChangeNicknameActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.project.R;
 
@@ -15,11 +19,20 @@ public class ChangeNicknameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_nickname);
 
-        Toolbar toolbar = findViewById(R.id.toolbar3);
+        Toolbar toolbar = findViewById(R.id.toolbar4);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//添加默认的返回图标
         getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+
+        final EditText new_nickname = findViewById(R.id.new_nickname);
+        Button btn_ensure_nickname = findViewById(R.id.ensure_nickname);
+        btn_ensure_nickname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ChangeNicknameActivity.this, "新昵称为："+ new_nickname.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
--- Project/app/src/main/java/com/example/project/activity/ChangePasswordActivity.java
+++ Project/app/src/main/java/com/example/project/activity/ChangePasswordActivity.java
@@ -1,8 +1,15 @@
 package com.example.project.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.project.R;
 
@@ -12,5 +19,30 @@ public class ChangePasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        Toolbar toolbar = findViewById(R.id.toolbar5);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//添加默认的返回图标
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+
+        final EditText old_password = findViewById(R.id.old_password);
+        final EditText new_password = findViewById(R.id.new_password);
+
+        Button ensure_password = findViewById(R.id.ensure_password);
+        ensure_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ChangePasswordActivity.this, "您输入的新密码是："+ new_password.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
